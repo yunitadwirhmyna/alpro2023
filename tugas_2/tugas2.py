50a7b0a3b052b3205057c4eedbcbc6dcb5e13e37
yunitadwirhmyna <yunita.22315023@umkendari.ac.id>
2023-11-25 16:18:32 +0700

    tugas 2 selesai

---
+++ tugas_2/tugas2.py
@@ -0,0 +1,15 @@
+
+
+if __name__ == "__main__":
+	# tupple
+	x = ( 1, 'Belimbing' , 3.14 , 'Ceri')
+	# list
+	y = [ 1, 'Belimbing' , 3,14 , 'ceri']
+	# dictionary
+	z = {1: 'Apel' , 'b': 'Belimbing' , 'c': 'Ceri' , 'x': 'Xemangka'}
+	print(type(x))
+	print(x)
+	print(type(y))
+	print(y)
+	print(type(z))
+	print(z)
